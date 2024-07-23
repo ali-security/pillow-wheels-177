6cf1256d3202a5876862dbe80a00bf414c393aa8
Ben Bader <ben.b@sealsecurity.io>
2024-07-23 10:47:49 +0300

    bbb

--- multibuild/docker_test_wrap.sh
+++ multibuild/docker_test_wrap.sh
@@ -6,24 +6,24 @@ echo "starting docker wrap test"
 # startup code in choose_python.sh, but the following are required and not
 # necessarily already set
 
-PYTHON_EXE=${PYTHON_EXE:-python}
-PIP_CMD=${PIP_CMD:-pip}
-echo "python exe: $PYTHON_EXE"
-echo "pip cmd: $PIP_CMD"
-# Get needed utilities
-MULTIBUILD_DIR=$(dirname "${BASH_SOURCE[0]}")
-source $MULTIBUILD_DIR/common_utils.sh
+# PYTHON_EXE=${PYTHON_EXE:-python}
+# PIP_CMD=${PIP_CMD:-pip}
+# echo "python exe: $PYTHON_EXE"
+# echo "pip cmd: $PIP_CMD"
+# # Get needed utilities
+# MULTIBUILD_DIR=$(dirname "${BASH_SOURCE[0]}")
+# source $MULTIBUILD_DIR/common_utils.sh
 
-# Change into root directory of repo
-cd /io
+# # Change into root directory of repo
+# cd /io
 
-# Configuration for this package in `config.sh`.
-# This can overwrite `install_run` and `install_wheel` (called from
-# `install_run`). These are otherwise defined in common_utils.sh.
-# `config.sh` must define `run_tests` if using the default `install_run`.
-CONFIG_PATH=${CONFIG_PATH:-config.sh}
-source "$CONFIG_PATH"
+# # Configuration for this package in `config.sh`.
+# # This can overwrite `install_run` and `install_wheel` (called from
+# # `install_run`). These are otherwise defined in common_utils.sh.
+# # `config.sh` must define `run_tests` if using the default `install_run`.
+# CONFIG_PATH=${CONFIG_PATH:-config.sh}
+# source "$CONFIG_PATH"
 
-echo "install run from docker"
-install_run
-echo "install run from docker done"
+# echo "install run from docker"
+# install_run
+# echo "install run from docker done"
